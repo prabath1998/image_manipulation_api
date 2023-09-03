2ef2c744da763f045070b8f42e0792c41f4a447f
prabath1998 <prabhathudayanga1998@gmail.com>
2023-09-03 15:48:31 +0530

    versioning

--- app/Http/Controllers/V1/AlbumController.php
+++ app/Http/Controllers/V1/AlbumController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\V1;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreAlbumRequest;
 use App\Http\Requests\UpdateAlbumRequest;
 use App\Models\Album;
