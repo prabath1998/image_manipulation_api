4646bef74b4d32afde3b7328011a3e7c6f622940
prabath1998 <prabhathudayanga1998@gmail.com>
2023-09-03 14:21:08 +0530

    create albums

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAlbumRequest $request)
+    {
+        $album =  Album::create($request->all());
+        return $album;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Album $album)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAlbumRequest $request, Album $album)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Album $album)
+    {
+        //
+    }
+}
